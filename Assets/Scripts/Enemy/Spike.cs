cbd33ae62303c3b42624d83adbf33dece4aeef48
peregurin-outscal <chinmay.virmani@outscal.com>
2023-12-06 17:34:24 +0530

    spike implementation

---
+++ Assets/Scripts/Enemy/Spike.cs
@@ -0,0 +1,12 @@
+using Platformer.Main;
+using Platformer.Player;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.GetComponent<PlayerView>() != null){
+            GameService.Instance.PlayerService.Die();
+        }
+    }
+}
