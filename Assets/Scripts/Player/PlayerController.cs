b6cd4466bd4150dd6078c94f55ffd6972789b790
peregurin-outscal <chinmay.virmani@outscal.com>
2024-02-06 13:28:41 +0530

    Refactored player view and player controller - moved all business logic from player view to player controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Platformer.InputSystem;
 using Platformer.Main;
 using UnityEngine;
@@ -13,11 +14,24 @@ namespace Platformer.Player
         private PlayerScriptableObject playerScriptableObject;
         public PlayerView PlayerView { get; private set; }
 
+        private PlayerStates playerState;
+        private float playerTranslateSpeed;
+
         #region Health
         private int currentHealth;
         public int CurrentHealth {
             get => currentHealth;
             private set => currentHealth = Mathf.Clamp(value, 0, playerScriptableObject.maxHealth);
+        }
+        #endregion
+
+        #region Grounded
+        private bool IsGrounded {
+            get {
+                var offset = 0.3f;
+                var raycastHit = Physics2D.Raycast(PlayerView.PlayerBoxCollider.bounds.center, Vector2.down, PlayerView.PlayerBoxCollider.bounds.extents.y + offset, PlayerView.GroundLayer);
+                return raycastHit.collider != null;
+            }
         }
         #endregion
 
@@ -35,14 +49,38 @@ namespace Platformer.Player
 
         private void InitializeVariables() => CurrentHealth = playerScriptableObject.maxHealth;
 
+        public void Update(){
+            var check = IsGrounded;
+            if(PlayerView.PlayerRigidBody.velocity.y < 0){
+                PlayerView.PlayerRigidBody.velocity += (GetFallMultiplier() - 1) * Physics2D.gravity.y * Time.deltaTime * Vector2.up;
+            }else if(PlayerView.PlayerRigidBody.velocity.y > 0){
+                PlayerView.PlayerRigidBody.velocity += (GetLowJumpMultiplier() - 1) * Physics2D.gravity.y * Time.deltaTime * Vector2.up;
+            }
+        }
+
+
         #region Handle Player Input
 
+        #region Player movement
         public void HandleHorizontalMovementAxisInput(float horizontalInput){
             var movementDirection = new Vector3(horizontalInput, 0f, 0f).normalized;
-            PlayerView.Move(horizontalInput, playerScriptableObject.movementSpeed);
+            MovePlayer(horizontalInput);
             PlayerService.MovePlayer(PlayerView.PlayerAnimator, movementDirection != Vector3.zero, PlayerView.Position);
         }
 
+        private void MovePlayer(float horizontalInput){
+            UpdateRunningStatus(horizontalInput);
+            if(horizontalInput != 0)
+                PlayerView.SetCharacterSpriteDirection(horizontalInput < 0);
+            if (playerState != PlayerStates.SLIDE) playerTranslateSpeed = playerScriptableObject.movementSpeed;
+            var movementVector = new Vector3(horizontalInput, 0.0f, 0.0f).normalized;
+            PlayerView.TranslatePlayer(playerTranslateSpeed * Time.deltaTime * movementVector);
+        }
+
+        private void UpdateRunningStatus(float horizontalInput) => playerState = horizontalInput != 0 ? PlayerStates.RUNNING : PlayerStates.IDLE;
+        #endregion
+
+        #region trigger input
         public void HandleTriggerInput(PlayerInputTriggers playerInputTriggers){
             // every case can have custom logic for movement or something else
             switch (playerInputTriggers)
@@ -63,28 +101,48 @@ namespace Platformer.Player
         }
 
         private void ProcessJumpInput(){
-            if(PlayerView.CanJump()){
+            if(CanJump()){
                 PlayerView.Jump(playerScriptableObject.jumpForce);
                 PlayerService.PlayJumpAnimation(PlayerView.PlayerAnimator);
             }
         }
 
+        private bool CanJump() => IsGrounded && (playerState == PlayerStates.IDLE || playerState == PlayerStates.RUNNING);
+
         private void ProcessAttackInput(){
-            if(PlayerView.CanAttack()){
-                PlayerView.Attack();
+            if(CanAttack()){
+                playerState = PlayerStates.ATTACK;
                 PlayerService.PlayAttackAnimation(PlayerView.PlayerAnimator);
             }
         }
 
+        private bool CanAttack() => IsGrounded && (playerState == PlayerStates.IDLE || playerState == PlayerStates.RUNNING);
+
         private void ProcessSlideInput(){
-            if(PlayerView.CanSlide()){
-                PlayerView.Slide(playerScriptableObject.slidingSpeed, playerScriptableObject.slidingTime);
+            if(CanSlide()){
+                Slide(playerScriptableObject.slidingSpeed, playerScriptableObject.slidingTime);
                 PlayerService.PlaySlideAnimation(PlayerView.PlayerAnimator);
             }
         }
 
+        private bool CanSlide() => IsGrounded && playerState == PlayerStates.RUNNING;
+
+        private async void Slide(float slidingSpeed, float slidingTime){
+            var temp = playerTranslateSpeed;
+            SetSlidingState(slidingSpeed, true);
+            await Task.Delay((int)(slidingTime * 1000));
+            SetSlidingState(temp, false);
+        }
+
+        private void SetSlidingState(float speed, bool isSliding)
+        {
+            playerTranslateSpeed = speed;
+            playerState = isSliding ? PlayerStates.SLIDE : PlayerStates.IDLE;
+        }
+        #endregion
         #endregion
 
+        #region Damage/Death
         public void Die() => PlayerService.PlayDeathAnimation(PlayerView.PlayerAnimator);
 
         public void TakeDamage(int damageToInflict)
@@ -100,11 +158,12 @@ namespace Platformer.Player
         }
 
         private void PlayerDied() => PlayerService.PlayerDied(PlayerView.PlayerAnimator);
+        #endregion
 
-        public float GetGravityDownForce() => playerScriptableObject.gravityDownForceMultiplier;
-
+        #region Getter Functions
         public float GetFallMultiplier() => playerScriptableObject.fallMultiplier;
 
         public float GetLowJumpMultiplier() => playerScriptableObject.lowJumpMultiplier;
+        #endregion
     }
 }
