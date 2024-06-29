93866d496077a6fa66759ebda3d4d361bbf7caaf
mridul-outscal <mridul.agrawal@outscal.com>
2024-06-29 17:36:27 +0530

    Created separate file for Player Animation Enums

---
+++ Assets/Scripts/Animation/PlayerAnimation.cs
@@ -0,0 +1,17 @@
+﻿namespace Platformer.AnimationSystem
+{
+    public enum PlayerAnimation
+    {
+        IDLE,
+        RUNNING
+    }
+    
+    public enum PlayerTriggerAnimation
+    {
+        JUMP,
+        SLIDE,
+        ATTACK,
+        TAKE_DAMAGE,
+        DEATH,
+    }
+}
