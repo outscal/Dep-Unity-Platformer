0936b1b216f0643c07d9a76c3313ffc6ad37aba4
mridul-outscal <mridul.agrawal@outscal.com>
2024-07-02 19:23:04 +0530

    Refactored PlayerScriptableObject.cs

--- Assets/Scripts/Player/PlayerScriptableObject.cs
+++ Assets/Scripts/Player/PlayerScriptableObject.cs
@@ -5,17 +5,22 @@ namespace Platformer.Player
     [CreateAssetMenu(fileName = "PlayerScriptableObject", menuName = "ScriptableObjects/PlayerScriptableObject")]
     public class PlayerScriptableObject : ScriptableObject
     {
+        // Initialization Data:
         public PlayerView prefab;
         public Vector3 spawnPosition;
         public Vector3 spawnRotation;
+        
+        // Player Attributes:
+        public int maxHealth;
         public float movementSpeed;
         public float slidingSpeed;
         public float slidingTime;
         public float jumpForce;
+        public int delayAfterDeath;
+        
+        // Multipliers:
         public float gravityDownForceMultiplier;
         public float fallMultiplier;
         public float lowJumpMultiplier;
-        public int maxHealth;
-        public int delayAfterDeath;
     }
 }
