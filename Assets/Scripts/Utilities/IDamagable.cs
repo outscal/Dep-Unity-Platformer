4e56a048c55b02426f5655d100aa640cdb2d700b
mridul-outscal <mridul.agrawal@outscal.com>
2024-07-03 14:13:21 +0530

    Reviewed IDamagable.cs

--- Assets/Scripts/Utilities/IDamagable.cs
+++ Assets/Scripts/Utilities/IDamagable.cs
@@ -1,4 +1,5 @@
 namespace Platformer.Utilities{
+    // TODO: The fact that Controllers are not implementing this interface is weird. Need a sync regarding this.
     public interface IDamagable{
         public void TakeDamage(int damage);
     }
