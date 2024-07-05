c7522a82cf619435720f60bf896d96fbabbaa279
mridul-outscal <mridul.agrawal@outscal.com>
2024-07-05 16:46:04 +0530

    refactored Spike.cs to not use PlayerView as IDamagable

--- Assets/Scripts/Enemy/Spike.cs
+++ Assets/Scripts/Enemy/Spike.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 public class Spike : MonoBehaviour
 {
-    // TODO: Will other enemies also take damage from this spike?
     [SerializeField] private int damage = 10;
+    
     private void OnTriggerEnter2D(Collider2D other){
-        if (other.TryGetComponent<IDamagable>(out var damagableObject))
+        if (other.TryGetComponent<PlayerView>(out var damagableObject))
         {
             damagableObject.TakeDamage(damage);
         }
