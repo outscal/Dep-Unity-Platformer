3251132e35b0f059ca7aae310799e790dbfd7cc9
mridul-outscal <mridul.agrawal@outscal.com>
2024-07-06 15:02:34 +0530

    refactored PlayerService.cs

--- Assets/Scripts/Player/PlayerService.cs
+++ Assets/Scripts/Player/PlayerService.cs
@@ -1,31 +1,13 @@
-using System.Collections;
-using System.Threading.Tasks;
-using Platformer.AnimationSystem;
-using Platformer.InputSystem;
-using Platformer.Main;
-using Platformer.Services;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
 namespace Platformer.Player
 {
     public class PlayerService
     {
-        private AnimationService AnimationService => GameService.Instance.AnimationService;
         public PlayerController playerController { get; private set; }
-
         
-        public PlayerService(PlayerScriptableObject playerScriptableObject)
-        {
-            SpawnPlayer(playerScriptableObject);
-        }
+        public PlayerService(PlayerScriptableObject playerScriptableObject) => SpawnPlayer(playerScriptableObject);
 
-        private void SpawnPlayer(PlayerScriptableObject playerScriptableObject) => 
-            playerController = new PlayerController(playerScriptableObject);
+        private void SpawnPlayer(PlayerScriptableObject playerScriptableObject) => playerController = new PlayerController(playerScriptableObject);
 
-        public void Update()
-        {
-            playerController.Update();
-        }
+        public void Update() => playerController.Update();
     }
 }
