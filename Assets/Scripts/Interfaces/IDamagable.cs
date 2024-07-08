bcb913e198fed02c4af033d665fcbfef6340e290
mridul-outscal <mridul.agrawal@outscal.com>
2024-07-08 11:23:45 +0530

    refactored IDamagable.cs removed TODOs from the script

--- Assets/Scripts/Interfaces/IDamagable.cs
+++ Assets/Scripts/Interfaces/IDamagable.cs
@@ -1,5 +1,4 @@
 namespace Platformer.Utilities{
-    // TODO: The fact that Controllers are not implementing this interface is weird. Need a sync regarding this.
     public interface IDamagable{
         public void TakeDamage(int damage);
     }
