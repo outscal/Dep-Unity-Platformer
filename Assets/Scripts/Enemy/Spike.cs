a62d514ff1fcd194ac72d7d1479dc5664168aa5f
mridul-outscal <mridul.agrawal@outscal.com>
2024-07-29 15:14:48 +0530

    refactored Spike.cs

--- Assets/Scripts/Enemy/Spike.cs
+++ Assets/Scripts/Enemy/Spike.cs
@@ -7,10 +7,9 @@ public class Spike : MonoBehaviour
 {
     [SerializeField] private int damage = 10;
     
-    private void OnTriggerEnter2D(Collider2D other){
+    private void OnTriggerEnter2D(Collider2D other)
+    {
         if (other.TryGetComponent<PlayerView>(out var damagableObject))
-        {
             damagableObject.TakeDamage(damage);
-        }
     }
 }
