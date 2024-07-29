467bd90c41e78490710d510145852a57bb2f3163
mridul-outscal <mridul.agrawal@outscal.com>
2024-07-29 15:23:05 +0530

    refactored IDamagable.cs

--- Assets/Scripts/Interfaces/IDamagable.cs
+++ Assets/Scripts/Interfaces/IDamagable.cs
@@ -1,5 +1,7 @@
-namespace Platformer.Utilities{
-    public interface IDamagable{
+namespace Platformer.Utilities
+{
+    public interface IDamagable
+    {
         public void TakeDamage(int damage);
     }
 }
